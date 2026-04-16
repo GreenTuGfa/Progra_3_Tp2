1c50fc2f310504d3234bcf1c3d4e22277c808557
GreenTuGfa <facundofer2607@gmail.com>
2026-04-16 19:43:57 -0300

    Creacion GeneradorConexiones

--- src/sistema/Conexion.java
+++ src/sistema/Conexion.java
@@ -4,10 +4,12 @@ public class Conexion {
 
 	Localidad origen;
 	Localidad destino;
+	double costo;
 	
-	public Conexion(Localidad origen, Localidad destino) {
+	public Conexion(Localidad origen, Localidad destino, double costo) {
 		
 		this.origen = origen;
 		this.destino = destino;
+		this.costo = costo;
 	}
 }
---
+++ src/sistema/GeneradorConexiones.java
@@ -0,0 +1,33 @@
+package sistema;
+import java.util.ArrayList;
+import java.util.List;
+
+import sistema.*;
+
+public class GeneradorConexiones {
+
+	CalculadorCosto calc;
+	
+	public GeneradorConexiones() {
+		calc = new CalculadorCosto(0, 0, 0);
+	}
+	
+	public List<Conexion> generarConexiones(List<Localidad> localidades) {
+		
+		List<Conexion> conexiones = new ArrayList<Conexion>();
+		
+		for (int i = 0; i < localidades.size(); i++) {
+			for (int j = i + 1; j < localidades.size(); j++) {
+				
+				Localidad a = localidades.get(i);
+				Localidad b = localidades.get(j);
+				
+				double costo = calc.calcular(a, b);
+				
+				Conexion conexion = new Conexion(a, b, costo);
+				conexiones.add(conexion);
+			}
+		}
+		return conexiones;
+	}
+}
