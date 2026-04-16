fe34b25e95d36cb090700217a77f6366bfc32ae7
GreenTuGfa <facundofer2607@gmail.com>
2026-04-16 18:17:48 -0300

    Test con distancias grandes (10.000km)

--- src/test/CalculadorDistanciaTest.java
+++ src/test/CalculadorDistanciaTest.java
@@ -47,6 +47,18 @@ class CalculadorDistanciaTest {
 		assertTrue(distancia_jcp_tig > distancia_jcp_pol);
 	}
 	@Test
+	
+	void distanciaGrande() {
+		Localidad bsas = new Localidad("Buenos Aires", "Argentina",-34.61144087673769, -58.42073845086299);
+		Localidad madr = new Localidad("Madrid", "España", 40.41064833747105, -3.5885587184389824);
+		CalculadorDistancia calc = new CalculadorDistancia();
+		
+		double distancia = calc.calcular(madr, bsas);
+		
+		assertTrue(distancia > 10000 && distancia < 10250); //La distancia entre buenos aires y madrid es aprox 10.000km
+	}
+	
+	@Test
 	void distanciaDosLocalidades() {
 		Localidad jcp = new Localidad("Jose C Paz", "Buenos Aires",-34.51394320352423,-58.76943963087526);
 		Localidad pol = new Localidad("Polvorines", "Buenos Aires",-34.49929991316347, -58.69336057451757);
