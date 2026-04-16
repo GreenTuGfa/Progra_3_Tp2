609cd9194de3be154231519087e7ad143a1d5484
GreenTuGfa <facundofer2607@gmail.com>
2026-04-16 15:23:30 -0300

    Add files via upload

---
+++ src/sistema/Conexion.java
@@ -0,0 +1,13 @@
+package sistema;
+
+public class Conexion {
+
+	Localidad origen;
+	Localidad destino;
+	
+	public Conexion(Localidad origen, Localidad destino) {
+		
+		this.origen = origen;
+		this.destino = destino;
+	}
+}
