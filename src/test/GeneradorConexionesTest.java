de8da11acccb1465c4086d868e58f7ce92403a8f
GreenTuGfa <facundofer2607@gmail.com>
2026-04-19 19:00:02 -0300

    Update GeneradorConexionesTest.java

--- src/test/GeneradorConexionesTest.java
+++ src/test/GeneradorConexionesTest.java
@@ -10,6 +10,15 @@ import sistema.*;
 
 class GeneradorConexionesTest {
 
+	List<Localidad>localidades = List.of(
+			new Localidad("a","",0,0), 
+			new Localidad("b","",0,0), 
+			new Localidad("c","",0,0),
+			new Localidad("d","",0,0));
+	
+	GeneradorConexiones generadorConexiones = new GeneradorConexiones();
+	
+	List<Conexion>conexiones = generadorConexiones.generarConexiones(localidades);
 	
 	@Test
 	void cantidadConexionesCorretas() {
@@ -17,29 +26,11 @@ class GeneradorConexionesTest {
 		/***
 		 * Cantidad de conexiones = n(n-1)/2. Con n siendo la cantidad de nodos (localidades)
 		 * */
-		List<Localidad>localidades = List.of(
-				new Localidad("a","",0,0), 
-				new Localidad("b","",0,0), 
-				new Localidad("c","",0,0),
-				new Localidad("d","",0,0));
-		
-		GeneradorConexiones generadorConexiones = new GeneradorConexiones();
-		
-		List<Conexion>conexiones = generadorConexiones.generarConexiones(localidades);
 		
 		assertEquals(conexiones.size(),6);
 	}
 	@Test
 	void conexionesSinLoops() {
-		List<Localidad>localidades = List.of(
-				new Localidad("a","",0,0), 
-				new Localidad("b","",0,0), 
-				new Localidad("c","",0,0),
-				new Localidad("d","",0,0));
-		
-		GeneradorConexiones generadorConexiones = new GeneradorConexiones();
-		
-		List<Conexion>conexiones = generadorConexiones.generarConexiones(localidades);
 		
 		for (Conexion conexion : conexiones) {
 			assertNotEquals(conexion.getOrigen(), conexion.getDestino());
